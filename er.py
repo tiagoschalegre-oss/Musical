1bf8568bcb5b59dd0feb79c6ab07d992f94b596a
Defiaaant <raileoncior41@gmail.com>
2026-06-11 11:43:30 -0300

    Base para prosseguimento do coding

---
+++ er.py
@@ -0,0 +1,35 @@
+from tkinter import*
+import pygame
+
+#----------------------------------------------------------------------
+
+def tocar_som():
+  nota =  pygame.mixer.Sound("Do (octave).wav")
+  nota.play()
+
+pygame.mixer.init()
+#----------------------------------------------------------------------
+
+root = Tk()
+root.geometry('600x600')
+
+img= PhotoImage(file='Chrollo-PNG-Photo.png')
+preta= PhotoImage(file='t_preta.png')
+root.iconphoto(True, img)
+root.config(background='#02021A')
+
+label=Label(root, bg='#02021A',
+              image= img,
+              compound='bottom')
+
+button=Button(root, image=preta,
+             bg='#000000',
+             width=30,
+             height=130,
+             activebackground='#000000',
+             activeforeground='#000000',
+             command=tocar_som)
+
+button.pack()
+
+root.mainloop()
