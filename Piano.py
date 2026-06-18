fd2c5d8bffa2ec994888044d8081f14b5e44717f
Defiaaant <raileoncior41@gmail.com>
2026-06-18 10:36:40 -0300

    "-"
    Updated sound file format for the Si button and removed background color for all buttons.

--- Piano.py
+++ Piano.py
@@ -19,49 +19,41 @@ root.iconphoto(True, img)
 root.config(background="#484886")
 
 Do = Button(root, image=preta,
-             bg='#000000',
              width=30, height=130,
              activebackground='#000000', activeforeground='#000000',
              command=lambda n=str("Do.wav"):tocar_som(n)).pack(side=LEFT)
 #------------------------------------------
 Re = Button(root, image=preta,
-             bg='#000000',
              width=30, height=130,
              activebackground='#000000', activeforeground='#000000',
              command=lambda n=str("Re.wav"):tocar_som(n)).pack(side=LEFT)
 #------------------------------------------
 Mi = Button(root, image=preta,
-             bg='#000000',
              width=30, height=130,
              activebackground='#000000', activeforeground='#000000',
              command=lambda n=str("Mi.wav"):tocar_som(n)).pack(side=LEFT)
 #------------------------------------------
 Fa = Button(root, image=preta,
-             bg='#000000',
              width=30, height=130,
              activebackground='#000000', activeforeground='#000000',
              command=lambda n=str("Fa.wav"):tocar_som(n)).pack(side=LEFT)
 #------------------------------------------
 Sol = Button(root, image=preta,
-             bg='#000000',
              width=30, height=130,
              activebackground='#000000', activeforeground='#000000',
              command=lambda n=str("Sol.wav"):tocar_som(n)).pack(side=LEFT)
 #------------------------------------------
 La = Button(root, image=preta,
-             bg='#000000',
              width=30, height=130,
              activebackground='#000000', activeforeground='#000000',
              command=lambda n=str("La.wav"):tocar_som(n)).pack(side=LEFT)
 #------------------------------------------
 Si = Button(root, image=preta,
-             bg='#000000',
              width=30, height=130,
              activebackground='#000000', activeforeground='#000000',
-             command=lambda n=str("Si.wav"):tocar_som(n)).pack(side=LEFT)
+             command=lambda n=str("Si.mp3"):tocar_som(n)).pack(side=LEFT)
 #------------------------------------------
 Do_ = Button(root, image=preta,
-             bg='#000000',
              width=30, height=130,
              activebackground='#000000', activeforeground='#000000',
              command=lambda n=str("Do#.wav"):tocar_som(n)).pack(side='left')
